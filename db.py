7086298b6b38011debe486901319ab88745782da
Manuel Fehren <Manuel.Fehren@Gmail.com>
2021-06-28 14:51:34 +0200

    Update db.py

--- db.py
+++ db.py
@@ -8,14 +8,15 @@ class database():
     db = TinyDB('data/database.json')
 
     def get_guild(self, id):
-        result = self.db.search(search_query.id == id)
+        result = self.db.table('guild').search(search_query.id == id)
         if len(result) == 0:
+            self.db.table('guild').insert({'id': id})
             return 0
         else:
             return result[0]
 
-    def update_guild(self, id, data : dict):
-        self.db.update({'data': data}, search_query.id == id)
+    def update_guild(self, id, field : str, data):
+        self.db.table('guild').upsert({field: data}, search_query.id == id)
 
     def create_homework(self, guild_id, subject : str, task : str, date):
         self.db.table('homework').insert({'guild': guild_id, 'subject': subject, 'task': task, 'date': date})
@@ -23,12 +24,12 @@ class database():
     def get_homework(self, id):
         try:
             return self.db.table('homework').search(search_query.guild == id)
-        except Exception:
-            print(Exception)
+        except Exception as e:
+            print(e)
             return []
 
-if __name__ == "__main__":
-    database = database()
-
-    guild = database.get_guild(857637783946526720)
-    print(guild)
+    def del_homework(self, id, date_time : str):
+        try:
+            self.db.table('homework').remove(search_query.date == date_time)
+        except Exception as e:
+            print(e)
